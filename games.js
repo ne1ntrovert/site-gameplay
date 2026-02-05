c1471e571084a06a7bd2f6eba6a0209c1d8c4ceb
Ilya Shironov <shironovilya777@gmail.com>
2026-02-05 14:23:57 +0300

    first commit

---
+++ games.js
@@ -0,0 +1,15 @@
+// function NumberGame() {
+//     const secretNumber = Math.floor(Math.random() * 100) + 1;
+//     while(true) {
+//         let guess = prompt("Угадай число от 1 до 100.")
+//         guess = Number(guess);
+//         if (guess === secretNumber) {
+//             alert("Поздравляю! Ты угадал(а). Игра завершена")
+//             break
+//         } else if (guess > secretNumber) {
+//             alert ("Попробуй снова) Слишком много")
+//         } else if (guess < secretNumber) {
+//             alert ("Попробуй снова! Слишком мало")
+//         }
+//     }
+// }
